35721c9d70ac9c157333585b32bc3e2b1563441a
David Langford <langford.d@gmail.com>
2021-03-02 14:04:09 +1000

    Fix tests

--- test/tech_experiments_1_web/controllers/page_controller_test.exs
+++ test/tech_experiments_1_web/controllers/page_controller_test.exs
@@ -1,8 +1,11 @@
 defmodule TechExperiments1Web.PageControllerTest do
+  @moduledoc """
+  Tests for the Page Controller
+  """
   use TechExperiments1Web.ConnCase
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Small Tech Experiments"
   end
 end
