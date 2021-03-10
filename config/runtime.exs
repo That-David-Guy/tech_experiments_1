41dde56f89e14da2561428a781e8db38907e0a6c
David Langford <langford.d@gmail.com>
2021-03-10 22:06:26 +1000

    Test rollback script 2

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,11 @@
 import Config
 # This file is run at runtime, not config time
 # Because of the way we deploy (double site behind nginx), we don't konw the port until runtime
+
+if (System.get_env("PORT") == nil) do
+  raise "Set the port when starting up. e.g. `PORT=4000 mix phx.server`"
+end
+
 config :tech_experiments_1, TechExperiments1Web.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT")),
